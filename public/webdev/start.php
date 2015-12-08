fced94ed6a71577473744d3579bafe4cbfe1876d
Oleg Minin <moveee@gmail.com>
2015-12-09 02:54:01 +0300

    wip

--- public/webdev/start.php
+++ public/webdev/start.php
@@ -225,7 +225,7 @@ class WebDev
 		
 		$ops_array=array('jcreate','jstart','jstop','jedit','jremove','jexport','jimport','madd','sstart','sstop');	//,'mremove'
 		$stat_array=array(
-			'jcreate'=>array('Creating...','Created'),
+			'jcreate'=>array('Creating...','Not running'),
 			'jstart'=>array('Starting','Launched'),
 			'jstop'=>array('Stopping','Stopped'),
 			'jedit'=>array('Saving','Saved'),
@@ -248,7 +248,6 @@ class WebDev
 				{
 					switch($op)
 					{
-						//case 'jcreate':	$task=array('task_id'=>0);break;
 						case 'jstart':	$res=$this->jailStart('jail'.$key,$key);break;
 						case 'jstop':	$res=$this->jailStop('jail'.$key,$key);break;
 						case 'jedit':	$res=$this->jailEdit('jail'.$key);break;
