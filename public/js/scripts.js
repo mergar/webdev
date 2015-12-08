0b7ad5b52a9c845e9d5c8535a152bc98dc66eff2
Oleg Minin <moveee@gmail.com>
2015-12-09 01:54:01 +0300

    wip

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1318,7 +1318,8 @@ status: "2"
 						this.jailsList=data.jails;
 						this.lastJailId=data.lastID;
 						this.showJailsList();
-						alert(data.lastID);
+						this.tasks.add({'operation':'jcreate','jail_id':data.lastID});
+						this.tasks.start();
 					}
 					break;
 				case 'modules':
