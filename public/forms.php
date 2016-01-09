4d717aa7c7f8ba4d4eff8bc2cde66cc02cf8a259
Oleg Minin <moveee@gmail.com>
2016-01-09 20:11:01 +0300

    wip

--- public/forms.php
+++ public/forms.php
@@ -107,7 +107,7 @@ h1 {color:white;background:silver;margin:0;padding:10px;}
 .form-field:last-child {padding-bottom:4px;}
 .form-field span {margin-left:10px;}
 .form-field input {width:300px;}
-form {border:1px solid gray;padding:0;margin-bottom:10px;width:500px;border-radius:8px;overflow:hidden;}
+form {border:1px solid gray;padding:0;margin-bottom:10px;width:500px;border-radius:8px;overflow:hidden;box-shadow:4px 4px 6px rgba(0,0,0,0.2);}
 </style>
 <?php
 $form->generate();
