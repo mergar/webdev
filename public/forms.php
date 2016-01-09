71d8dce0022c6bfbe98e421a0ce6f0b88e8d1c36
Oleg Minin <moveee@gmail.com>
2016-01-09 20:12:01 +0300

    wip

--- public/forms.php
+++ public/forms.php
@@ -103,7 +103,7 @@ $form=new Forms('php');
 body {font-size:100%;font-family:Tahoma,'Sans-Serif',Arial;}
 h1 {color:white;background:silver;margin:0;padding:10px;}
 .small {font-size:x-small;}
-.form-field {padding:4px 4px 0 4px;margin:0 4px; background:#fafafa;}
+.form-field {padding:4px 4px 0 4px;margin:0 4px; background:#fffafa;}
 .form-field:last-child {padding-bottom:4px;}
 .form-field span {margin-left:10px;}
 .form-field input {width:300px;}
