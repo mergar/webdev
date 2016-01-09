c3de31fb492d2c16fcc98bb6a5e6280b400e5367
Oleg Minin <moveee@gmail.com>
2016-01-09 19:31:01 +0300

    wip

--- public/forms.php
+++ public/forms.php
@@ -100,7 +100,8 @@ $form=new Forms('php');
 <style>
 body {font-size:100%;font-family:Tahoma,'Sans-Serif',Arial;}
 .small {font-size:x-small;}
-.form-field {padding:4px;margin:0 4px; background:#f0f0f0;}
+.form-field {padding:4px 4px 0 4px;margin:0 4px; background:#f0f0f0;}
+.form-field:last-child {padding-bottom:4px;}
 .form-field span {margin-left:10px;}
 </style>
 <?php
