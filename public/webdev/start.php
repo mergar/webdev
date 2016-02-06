0463f789af52e21563c04ba96b051b9959f6c645
Oleg Minin <moveee@gmail.com>
2016-02-07 01:20:01 +0300

    wip

--- public/webdev/start.php
+++ public/webdev/start.php
@@ -1083,7 +1083,7 @@ class WebDev
 				{
 					//$jails[$key]['ip']=$old['ip4_addr'];
 					$query="update jails set ip='".$old['ip4_addr']."' where id=".$j['id'];
-					$res=$this->_db->update($query);
+					$res=$this->_db_jails->update($query);
 				}
 			}
 			
