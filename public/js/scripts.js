51dbb0c6bdd6f2a2f3a04739fc4281cf8daca757
Oleg Minin <moveee@gmail.com>
2016-02-16 01:36:01 +0300

    wip

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -924,8 +924,8 @@ status: "2"
 		{
 			var cl=target.className;
 			if(cl=='') return;
-			if(cl='save-helper-values') this.saveHelperValues();
-			if(cl='clear-helper') this.clearHelperForm(target);
+			if(cl=='save-helper-values') this.saveHelperValues();
+			if(cl=='clear-helper') this.clearHelperForm(target);
 			
 			return;
 		}
